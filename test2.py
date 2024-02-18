7d70aee4adc471cf08ed7219f6b10feaffd65020
Snusl1k <tkacenkosasa355@gmail.com>
2024-02-18 23:24:06 +0200

    almost completed codeS

--- test2.py
+++ test2.py
@@ -1,29 +1,41 @@
-#import csv
-#import random
-
-# Чтение данных из CSV-файла
-#with open(r'C:\python\Project\taro.csv', 'r', encoding='utf-8') as csvfile:
-#    reader = csv.reader(csvfile)
-#    next(reader)  # Пропустить заголовок, если он есть
-#    card_values = [row[0] for row in reader]
-
-# Рандомный выбор значения
-#random_card_value = random.choice(card_values)
-
-#print(f'Рандомная карта: {random_card_value}')
 
 import csv
 import random
+import gspread
+from google.oauth2 import service_account
+from gspread.exceptions import APIError
+
+
+
+credentials_path = 'C:\\Users\\Tkach\\Downloads\\glass-ally-414719-d0c89a69ae01.json'
+
+
+credentials = service_account.Credentials.from_service_account_file(
+    credentials_path,
+    scopes=['https://www.googleapis.com/auth/spreadsheets']
+)
+spreadsheet_id = '1cjoEtpVmRHn8CXZDJWtXkd86bVgmsXjqw5FeNIiay2E'
+client = gspread.authorize(credentials)
+spreadsheet = client.open_by_key(spreadsheet_id)
+
+
 
 def get_value(combination):
-    with open(r'C:\python\Project\taro.csv', 'r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
+    try:
+        # Отримання аркуша за замовчуванням
+        worksheet = spreadsheet.sheet1
+
+        # Отримання всіх записів у вигляді списку словників, де ключі - це назви стовпців
+        all_records = worksheet.get_all_records()
+
+        for row in all_records:
             if row['Комбінації карт'] == combination:
                 return row['Значення комбінацій карт']
 
-    return 'Value not found'
-
+        return 'Value not found'
+    except APIError as e:
+        print(f'APIError: {e.response}')
+        return 'APIError'
 # Random combination
 random_combination = "Суд and Десятка чаш"
 
