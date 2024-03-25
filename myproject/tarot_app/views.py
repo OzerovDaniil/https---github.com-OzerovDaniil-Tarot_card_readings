c71ffb68bff63ebbda7d380d47a5ec15f181ad9c
Daniil <danil20052309@gmail.com>
2024-03-26 01:21:34 +0200

    don't_work_don't_reading_JSON

--- myproject/tarot_app/views.py
+++ myproject/tarot_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 import random
 import nltk
 from nltk import word_tokenize
 import gspread
 from google.oauth2 import service_account
 from gspread.exceptions import APIError
+from django.http import JsonResponse
 import time
+from django.http import JsonResponse
+
+def my_view(request):
+    # Отримання даних для відповіді
+    data = {'key': 'value'}
+    # Повернення даних у форматі JSON
+    return JsonResponse(data)
 
 nltk.download('punkt')
 
@@ -21,70 +26,78 @@ def about(request):
 def taro(request):
     return render(request, 'tarot_app/taroreading.html')
 
-# def question_check(user_question):
-#     words = word_tokenize(user_question)
-#     if words[-1] == '?' and len(words) > 1:
-#         return True
-#     return False
+def question_check(user_question):
+    words = word_tokenize(user_question)
+    if words[-1] == '?' and len(words) > 1:
+        return True
+    return False
 
-# def open_question_check(user_question):
-#     open_key_words = ["Що", "Чому", "Як", "Які", "Яка", "Яке"]
-#     words = word_tokenize(user_question)
-#     for key_word in open_key_words:
-#         if key_word in words:
-#             return True
-#     return False
+def open_question_check(user_question):
+    open_key_words = ["Що", "Чому", "Як", "Які", "Яка", "Яке", "що", "чому", "як", "які", "яка", "яке"]
+    words = word_tokenize(user_question)
+    for key_word in open_key_words:
+        if key_word in words:
+            return True
+    return False
 
-# class TarotMeanings:
-#     def predict(self, cards):
-#         return ["Chto to poka chto"]
+def get_random_combination():
+    try:
+        credentials_path = 'glass-ally-414719-d0c89a69ae01.json'
+        credentials = service_account.Credentials.from_service_account_file(credentials_path, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+        spreadsheet_id = '1cjoEtpVmRHn8CXZDJWtXkd86bVgmsXjqw5FeNIiay2E'
+        client = gspread.authorize(credentials)
+        spreadsheet = client.open_by_key(spreadsheet_id)
 
-# Tarot_Cards = ["Жрець", "Блазень", "Маг", "Жриця", "Імператриця", "Імператор", "Ієрофант", "Закохані", "Колісниця", "Сила", "Пустельник", "Колесо Фортуни", "Справедливість", "Повішений", "Смерть", "Помірність", "Диявол", "Башта", "Зірка", "Місяць", "Сонце", "Страшний суд", "Світ", "Туз жезлів", "Двійка жезлів", "Трійка жезлів", "Четвірка жезлів", "П’ятірка жезлів", "Шістка жезлів", "Сімка жезлів", "Вісімка жезлів", "Дев’ятка жезлів", "Десятка жезлів", "Паж жезлів", "Лицар жезлів", "Королева жезлів", "Король жезлів", "Туз мечів", "Двійка мечів", "Трійка мечів", "Четвірка мечів", "П’ятірка мечів", "Шістка мечів", "Сімка мечів", "Вісімка мечів", "Дев’ятка мечів", "Десятка мечів", "Паж мечів", "Лицар мечів", "Королева мечів", "Король мечів", "Туз пентаклей", "Двійка пентаклей", "Трійка пентаклей", "Четвірка пентаклей", "П’ятірка пентаклей", "Шістка пентаклей", "Сімка пентаклей", "Вісімка пентаклей", "Дев’ятка пентаклей", "Десятка пентаклей", "Паж пентаклей", "Лицар пентаклей", "Королева пентаклей", "Король пентаклей"]
+        worksheet = spreadsheet.sheet1
+        all_records = worksheet.get_all_records()
 
-# meanings = TarotMeanings()
+        # Отримуємо випадковий рядок
+        random_row = random.choice(all_records)
 
-# def get_combination_for_pair(card1, card2):
-#     try:
-#         credentials_path = 'glass-ally-414719-d0c89a69ae01.json'
-#         credentials = service_account.Credentials.from_service_account_file(credentials_path, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-#         spreadsheet_id = '1cjoEtpVmRHn8CXZDJWtXkd86bVgmsXjqw5FeNIiay2E'
-#         client = gspread.authorize(credentials)
-#         spreadsheet = client.open_by_key(spreadsheet_id)
+        # Отримуємо значення двох стовпців для випадкового рядка
+        combination = random_row['Комбінації карт']
+        value = random_row['Значення комбінацій карт']
 
-#         worksheet = spreadsheet.sheet1
-#         all_records = worksheet.get_all_records()
+        return combination, value
+    except Exception as e:
+        print(f'Error: {e}')
+        return None, None
 
-#         for row in all_records:
-#             if row['Комбінації карт'] == search_cards:
-#                 return row['Значення комбінацій карт']
-#     except APIError as e:
-#         print(f'APIError: {e.response}')
-#         return 'APIError'
 
-# while True:
-#     user_question = input("Enter your question: ")
-#     if question_check(user_question) is False:
-#         print("Помилка, ви не поставили знак питання, або написали меньше одного слова")
-#     elif question_check(user_question) is True:
-#         if open_question_check(user_question) is False:
-#             print("Помилка, ви задали не відкрите питання")
-#         elif open_question_check(user_question) is True:
-#             break
+def your_view(request):
+    if request.method == 'POST':
+        user_question = request.POST.get('tarot_query', '')
+        if question_check(user_question):
+            if open_question_check(user_question):
+                context = {
+                    'question_type': 'Відкрите питання',
+                    'user_question': user_question,
+                }
+                return render(request, 'query_result.html', context)
+            else:
+                context = {
+                    'question_type': 'Закрите питання',
+                    'user_question': user_question,
+                }
+                return render(request, 'query_result.html', context)
+        else:
+            error_message = ''
+            if len(user_question) < 1:
+                error_message = 'Помилка: ви не ввели запитання.'
+            elif not user_question.endswith('?'):
+                error_message = 'Помилка: запитання має закінчуватися знаком питання.'
+            return render(request, 'taroreading.html', {'error_message': error_message})
+    else:
+        combination, value = get_random_combination()
 
-# # Рандомний вибір двох карт зі списку
-# while True: 
-#     random_cards = random.sample(Tarot_Cards, 2)
-#     card1, card2 = random_cards
-  
-#     search_cards = card1 + " " + "and" + " " + card2
-
-#     # Пошук комбінації для кожної пари карт
-#     combination = get_combination_for_pair(card1, card2)
-
-#     # Додайте затримку між ітераціями, якщо потрібно
-#     time.sleep(2)
-#     print('\n') 
-    
-#     if combination:
-#         print(f'Комбінація для пари {card1} і {card2}: {combination}')
-#         break
+        if combination and value:
+            context = {
+                'question_type': '...',
+                'user_question': '...',
+                'combination': combination,
+                'value': value,
+            }
+            if combination and value:
+                return JsonResponse({'result': f'Combination: {combination}, Value: {value}'})
+            else:
+                return JsonResponse({'error': 'Error message here'}, status=400)
